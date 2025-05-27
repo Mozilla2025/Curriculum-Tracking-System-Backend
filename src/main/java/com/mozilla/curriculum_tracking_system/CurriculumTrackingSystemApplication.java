7491b470333d3a0127ec4ba7319eee2a8e9259d2
newton <wamitinewton@gmail.com>
2025-05-27 06:59:49 -0400

    init

---
+++ src/main/java/com/mozilla/curriculum_tracking_system/CurriculumTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.mozilla.curriculum_tracking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurriculumTrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurriculumTrackingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mozilla/curriculum_tracking_system/CurriculumTrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mozilla.curriculum_tracking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurriculumTrackingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
