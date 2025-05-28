1996f1cc2b619e617da43ff0fb5085479b66c264
Phenny Mwaisaka <mwaisakaphenny@gmail.com>
2025-05-28 13:22:48 +0300

    Custom UserDetailsService Implementation

---
+++ src/main/java/com/mozilla/curriculum_tracking_system/security/UserDetailsImpl.java
@@ -0,0 +1,61 @@
+package com.mozilla.curriculum_tracking_system.security;
+
+import com.mozilla.curriculum_tracking_system.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+
+    private long Id;
+    private String username;
+    private String email;
+    private String password;
+    private Boolean isActive;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public static UserDetailsImpl build(User user){
+        List<GrantedAuthority> authorities = user.getRoles().stream()
+                .map(roles -> new SimpleGrantedAuthority("ROLE_" + roles.getName()))
+                .collect(Collectors.toList());
+
+        return new UserDetailsImpl(
+                user.getId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getPassword_hash(),
+                user.getIs_active(),
+                authorities
+        );
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isActive;
+    }
+}
