bb91d5eafe2bb772381b0a3c01db1dcf7fb79974
newton <wamitinewton@gmail.com>
2025-06-14 12:29:58 +0300

    updated dependency management

--- src/main/java/com/mozilla/curriculum_tracking_system/repository/school/SchoolRepository.java
+++ src/main/java/com/mozilla/curriculum_tracking_system/repository/school/SchoolRepository.java
@@ -1,14 +1,8 @@
 package com.mozilla.curriculum_tracking_system.repository.school;
 
 import com.mozilla.curriculum_tracking_system.model.school.School;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface SchoolRepository extends JpaRepository<School, Long> {
