d1dc214b9e74017d4944457dfd0c79a68eb8b892
Newton Wamiti <144038101+Wamitinewton@users.noreply.github.com>
2025-07-04 19:58:34 +0300

    school services

---
+++ src/main/java/com/mozilla/curriculum_tracking_system/dto/school/SchoolDto.java
@@ -0,0 +1,18 @@
+package com.mozilla.curriculum_tracking_system.dto.school;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SchoolDto {
+
+    private String name;
+    private String code;
+    private Long deanId;
+
+}
---
+++ src/main/java/com/mozilla/curriculum_tracking_system/service/school/SchoolService.java
@@ -0,0 +1,52 @@
+package com.mozilla.curriculum_tracking_system.service.school;
+
+
+import com.mozilla.curriculum_tracking_system.dto.school.SchoolDto;
+import com.mozilla.curriculum_tracking_system.exception.ResourceNotFoundException;
+import com.mozilla.curriculum_tracking_system.mapper.SchoolMapper;
+import com.mozilla.curriculum_tracking_system.model.school.School;
+import com.mozilla.curriculum_tracking_system.repository.school.SchoolRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class SchoolService implements ISchoolService{
+
+    private final SchoolRepository schoolRepository;
+    private final SchoolMapper schoolMapper;
+
+    @Override
+    public List<SchoolDto> getAllSchools() {
+        log.debug("Fetching all schools from database");
+        List<School> schools = schoolRepository.findAll();
+        List<SchoolDto> schoolDtos = schools.stream().map(schoolMapper::mapToDto).toList();
+        log.debug("Retrieved {} schools from database", schools);
+        return schoolDtos;
+    }
+
+    @Override
+    public SchoolDto getSchoolById(Long id) {
+        log.debug("fetching school with ID: {}", id);
+
+        if (id == null){
+            throw new IllegalArgumentException("School ID cannot be null");
+        }
+
+        School school = schoolRepository.findById(id)
+                .orElseThrow(() -> {
+                    log.warn("School not found with ID: {}", id);
+                    return new ResourceNotFoundException("School not found with ID: " + id);
+                });
+        return schoolMapper.mapToDto(school);
+    }
+
+
+}
+
+
