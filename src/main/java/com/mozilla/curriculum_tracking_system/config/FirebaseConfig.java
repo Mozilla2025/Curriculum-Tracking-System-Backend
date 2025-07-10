b903f87b606b54b536b06624a336c1a713f0e928
Newton Wamiti <144038101+Wamitinewton@users.noreply.github.com>
2025-07-10 19:15:31 +0300

    firebase config

---
+++ src/main/java/com/mozilla/curriculum_tracking_system/config/FirebaseConfig.java
@@ -0,0 +1,34 @@
+package com.mozilla.curriculum_tracking_system.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+@Configuration
+@Slf4j
+public class FirebaseConfig {
+
+    @Value("${firebase.credentials.path}")
+    private String credentialsPath;
+
+
+    @Bean
+    public Storage initializeFirebase() throws Exception {
+        try {
+            log.info("Initializing firebase with credentials: {}", credentialsPath);
+            GoogleCredentials credentials = GoogleCredentials
+                    .fromStream(new ClassPathResource(credentialsPath).getInputStream());
+            return StorageOptions.newBuilder()
+                    .setCredentials(credentials)
+                    .build()
+                    .getService();
+        } catch (Exception e) {
+            log.error("Error initializing firebase storage", e);
+            throw e;
+        }
+    }
+}
